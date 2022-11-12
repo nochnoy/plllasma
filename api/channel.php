f2d02b0bf0666d4f1ab0a9bc0b21ccd1f84ad2e5
nochnoy <no-subject@yandex.ru>
2022-11-12 21:54:12 +0300

    Просмотренные каналы получают lastViewed

--- api/channel.php
+++ api/channel.php
@@ -12,6 +12,12 @@ if (!canRead($placeId)) {
     die('{"error": "access"}');
 }
 
-exit('{"id":'.$placeId.', "messages":'.getChannelJson($placeId, $lastViewed).'}');
+$result = '{"id":'.$placeId.', "messages":'.getChannelJson($placeId, $lastViewed).'}';
 
+// Помечаем канал как просмотренный
+$sql = $mysqli->prepare('UPDATE lnk_user_place SET time_viewed=NOW() WHERE id_place=? AND id_user=?');
+$sql->bind_param("ii", $placeId, $user['id_user']);
+$sql->execute();
+
+exit($result);
 ?>
