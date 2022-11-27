e6c4ed110c3d2c52dc2c6231dfa6373b05b83df9
nochnoy <no-subject@yandex.ru>
2022-11-27 23:41:18 +0300

    Бэкграунд страницы. Охуенный логин скрин.

--- frontend/src/app/pages/login-page/login-page.component.ts
+++ frontend/src/app/pages/login-page/login-page.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import {FormGroup, FormControl, RequiredValidator, Validators} from '@angular/forms';
 import {UntilDestroy, untilDestroyed} from "@ngneat/until-destroy";
 import {UserService} from "../../services/user.service";
 import {AppService} from "../../services/app.service";
+import {tap} from "rxjs/operators";
 
 @UntilDestroy()
 @Component({
@@ -10,30 +11,39 @@ import {AppService} from "../../services/app.service";
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent {
 
   constructor(
     public appService: AppService,
-    public userService: UserService,
   ) { }
 
   form = new FormGroup({
-    login: new FormControl(''),
-    password: new FormControl(''),
+    login: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
   });
 
-  ngOnInit(): void {
-    this.buildForm();
-  }
-
-  buildForm(): void {
-
-  }
+  authorizing = false;
+  error = 0;
 
   onLogin(): void {
+    this.authorizing = true;
+    this.form.markAsPristine();
+    this.form.disable();
+
     const login = this.form.get('login')?.value ?? '';
     const password = this.form.get('password')?.value ?? '';
     this.appService.login$(login, password).pipe(
+      tap((result) => {
+        this.authorizing = false;
+        this.form.enable();
+        if (result) {
+          this.error = 0;
+        } else {
+          if (++this.error > 4) {
+            this.error = 1;
+          }
+        }
+      }),
       untilDestroyed(this)
     ).subscribe();
   }
