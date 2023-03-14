fcd0739abd598eeabaffe60406dac1bba0c8e4d7
nochnoy <no-subject@yandex.ru>
2023-03-14 22:35:40 +0300

    Сокращалку серых веток пофиксил

--- frontend/src/app/modules/shared/pipes/shorten.pipe.ts
+++ frontend/src/app/modules/shared/pipes/shorten.pipe.ts
@@ -4,12 +4,12 @@ import Autolinker, {AutolinkerConfig, ReplaceFnReturn, TruncateConfig} from 'aut
 @Pipe({ name: 'shorten' })
 export class ShortenPipe implements PipeTransform {
 
-  static maxLength = 80;
-  static divider = ' ……… ';
+  static maxLength = 220;
+  static divider = ' … … … ';
 
   transform(value: string): string {
     if (value.length > ShortenPipe.maxLength + ShortenPipe.divider.length) {
-      const half = Math.floor(value.length / 2);
+      const half = Math.floor(ShortenPipe.maxLength / 2);
       const start = value.substring(0, half);
       const finish = value.substring(value.length - half, value.length);
       return `${start}${ShortenPipe.divider}${finish}`;
