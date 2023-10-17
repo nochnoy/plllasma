e97181ddaa11b936c465338bf836980ccbfb9df5
nochnoy <no-subject@yandex.ru>
2023-10-17 22:47:15 +0300

    Страница каналов

--- api/channels-list.php
+++ api/channels-list.php
@@ -8,8 +8,8 @@ loginBySessionOrToken();
 $sql =
     'SELECT DISTINCT p.id_place, p.parent, p.first_parent, p.name, p.description, p.time_changed, p.path, p.typ, l.weight, l.time_viewed, l.at_menu, a.role'.
     ' FROM tbl_places p'.
-    ' LEFT JOIN lnk_user_place l ON l.id_place = p.id_place AND l.id_user = '.$user['id_user'].
-    ' LEFT JOIN tbl_access a ON a.id_place = p.id_place AND a.id_user = '.$user['id_user']
+    ' LEFT JOIN tbl_access a ON a.id_place = p.id_place AND a.id_user = '.$user['id_user'].    
+    ' LEFT JOIN lnk_user_place l ON l.id_place = p.id_place AND l.id_user = '.$user['id_user']
     ;
 $result = mysqli_query($mysqli, $sql);
 
