115613cbb01594baeb671165e6aef67de3ca1485
nochnoy <no-subject@yandex.ru>
2023-12-03 19:59:40 +0300

    Новый год - анимация ёлочки в процессе

--- frontend/src/app/components/newyear/newyear.component.ts
+++ frontend/src/app/components/newyear/newyear.component.ts
@@ -9,7 +9,12 @@ export class NewyearComponent implements OnInit {
 
   constructor() { }
 
+  act = 0;
+
   ngOnInit(): void {
+    setTimeout(() => this.act = 1, 1 * 1000);
+    setTimeout(() => this.act = 2, 8 * 1000);
+    setTimeout(() => this.act = 3, 15 * 1000);
   }
 
 }
