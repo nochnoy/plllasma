c31bb01a7486eaa0393775d4f5b54606daec8394
nochnoy <no-subject@yandex.ru>
2023-12-10 23:45:18 +0300

    Новый год - анимация ёлочки в процессе

--- frontend/src/app/components/newyear/newyear.component.ts
+++ frontend/src/app/components/newyear/newyear.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {of} from "rxjs";
-import {delay, repeat, switchMap, tap} from "rxjs/operators";
+import {concat, from, of} from "rxjs";
+import {concatMap, delay, flatMap, repeat, switchMap, tap} from "rxjs/operators";
 import {CookieService} from "ngx-cookie-service";
 import {UntilDestroy, untilDestroyed} from "@ngneat/until-destroy";
 
@@ -16,11 +16,23 @@ export class NewyearComponent implements OnInit {
     public cookieService: CookieService
   ) { }
 
+  // Массив кадров, в каждом кадре - номер цвета для каждого из 4х пятен. Цвет 0 = не горит.
+  // Позиции в кадре: верх-лево, низ-лево, вехр-право, низ-право
+  // Числа в кадре: 0-темнота, 1-красный, 2-синий, 3-сереневый, 4-жёлтый
+  readonly glowTune = [
+    [4, 0, 0, 5],
+    [0, 3, 1, 1],
+    [3, 2, 0, 0],
+    [0, 1, 4, 4],
+    [0, 2, 1, 4],
+    [0, 3, 0, 1],
+    [0, 4, 0, 4],
+    [1, 0, 3, 0],
+  ];
+
   act = 0;
-  isRedOn = false;
-  isBlueOn = false;
-  isGreenOn = false;
-  isGoldOn = false;
+  glowColors: number[] = [];
+  glowFast = true;
 
   ngOnInit(): void {
     const year = (new Date()).getFullYear() + '';
@@ -41,33 +53,27 @@ export class NewyearComponent implements OnInit {
           );
         }
       }),
-      tap(() => this.act = 4), // Гирлянды
+      tap(() => this.act = 4), // Играют гирлянды
       switchMap(() => {
         return of({}).pipe(
-          delay(1000 * 2),
           tap(() => {
-            let count = 2;
-            let lights: number[] = [];
-            if (Math.random() > 0.6) {
-              count = 3;
-            }
-            while(lights.length < count) {
-              const l = Math.floor(Math.random() * 4 + 1);
-              if (!lights.includes(l)) {
-                lights.push(l);
-              }
-            }
-            this.isRedOn = lights.includes(1);
-            this.isBlueOn = lights.includes(2);
-            this.isGreenOn = lights.includes(3);
-            this.isGoldOn = lights.includes(4);
+            this.glowFast = !this.glowFast;
+          }),
+          switchMap(() => from(this.glowTune).pipe(concatMap(x => of(x).pipe(delay(this.glowFast ? 1000 * 0.3 : 1000 * 2.5))))),
+          tap((rec) => {
+            rec.forEach((color, id) => {
+              this.glowColors[id] = color;
+            })
           }),
           repeat(),
         );
       }),
-
       untilDestroyed(this)
     ).subscribe();
+  }
+
+  getGlowClass(id: number): string {
+    return `glow glow-${id} glow-color-${(this.glowColors[id] ?? 0)}`;
   }
 
 }
